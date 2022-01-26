5ce6d6ac8064e0c1405a16695505c845e3e5ac91
Shawn Dermer <shawn.dermer@gmail.com>
2022-01-26 16:15:43 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
